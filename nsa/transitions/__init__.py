b20e206370fb71bd174690de150f7d481384e8f4
Adam Lapworth <adamlap@hotmail.com>
2026-08-19 01:54:30 +0200

    feat: add NSA transition engine package

---
+++ nsa/transitions/__init__.py
@@ -0,0 +1,5 @@
+"""Algebra-preserving state transitions for NSA."""
+
+from .engine import TransitionEngine, TransitionPolicy, TransitionResult
+
+__all__ = ["TransitionEngine", "TransitionPolicy", "TransitionResult"]
