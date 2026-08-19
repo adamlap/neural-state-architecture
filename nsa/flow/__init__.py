292f3f2d49431a036ca98723c90b0d8dcd74006a
Adam Lapworth <adamlap@hotmail.com>
2026-08-19 02:03:11 +0200

    feat: add NSA information-flow package

---
+++ nsa/flow/__init__.py
@@ -0,0 +1,5 @@
+"""Whole-system NSA information-flow primitives."""
+
+from .graph import FlowEdge, FlowGraph, FlowNode, FlowViolation
+
+__all__ = ["FlowEdge", "FlowGraph", "FlowNode", "FlowViolation"]
