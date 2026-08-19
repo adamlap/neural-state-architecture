14edd149a39f496aadc425a6f62ac88562628473
Adam Lapworth <adamlap@hotmail.com>
2026-08-19 02:10:35 +0200

    feat: export predictive self-state API

--- nsa/self_state/__init__.py
+++ nsa/self_state/__init__.py
@@ -1,5 +1,11 @@
 """Self-state and metacognitive primitives for NSA research."""
 
 from .model import SelfState, SelfStateObservation
+from .prediction import SelfStatePrediction, SelfStatePredictor
 
-__all__ = ["SelfState", "SelfStateObservation"]
+__all__ = [
+    "SelfState",
+    "SelfStateObservation",
+    "SelfStatePrediction",
+    "SelfStatePredictor",
+]
