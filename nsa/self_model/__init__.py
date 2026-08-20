dcc9c258628ae1b896644e32324f88f47d164cc0
Adam Lapworth <adamlap@hotmail.com>
2026-08-21 01:09:16 +0200

    feat(phase19): expose self-model trajectory API

---
+++ nsa/self_model/__init__.py
@@ -0,0 +1,5 @@
+"""Phase 19 self-model and live trajectory primitives."""
+
+from .trajectory import ActionFeatures, TrajectoryStep, trajectory_step
+
+__all__ = ["ActionFeatures", "TrajectoryStep", "trajectory_step"]
