ad35205d794235d8a8c378a94292f7fb925058d0
Adam Lapworth <adamlap@hotmail.com>
2026-08-20 16:46:07 +0200

    feat(runtime): export typed NSA runtime

--- nsa/runtime/__init__.py
+++ nsa/runtime/__init__.py
@@ -5,5 +5,6 @@ Trusted Cognitive Runtime for NSA Autonomous Execution.
 """
 
 from .engine import CognitiveRuntime, ExecutionContext
+from .typed_runtime import NSATypedRuntime, RuntimeGeneration
 
-__all__ = ["CognitiveRuntime", "ExecutionContext"]
+__all__ = ["CognitiveRuntime", "ExecutionContext", "NSATypedRuntime", "RuntimeGeneration"]
