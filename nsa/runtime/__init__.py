942c6339f449ffbe505b26c2256776aad8697121
Adam Lapworth <adamlap@hotmail.com>
2026-08-21 21:49:54 +0200

    feat(runtime): export CCE substrate binding

--- nsa/runtime/__init__.py
+++ nsa/runtime/__init__.py
@@ -4,7 +4,18 @@ nsa.runtime
 Trusted Cognitive Runtime for NSA Autonomous Execution.
 """
 
+from .cce_adapter import SubstrateTransition, SubstrateTransitionConfig
+from .continuous_engine import CCEStatus, ContinuousCognitiveEngine
 from .engine import CognitiveRuntime, ExecutionContext
 from .typed_runtime import NSATypedRuntime, RuntimeGeneration
 
-__all__ = ["CognitiveRuntime", "ExecutionContext", "NSATypedRuntime", "RuntimeGeneration"]
+__all__ = [
+    "CCEStatus",
+    "ContinuousCognitiveEngine",
+    "CognitiveRuntime",
+    "ExecutionContext",
+    "NSATypedRuntime",
+    "RuntimeGeneration",
+    "SubstrateTransition",
+    "SubstrateTransitionConfig",
+]
