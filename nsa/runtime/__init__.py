8a1a17a1190553e5c1a357eea8ad53ff7b9070f8
Adam Lapworth <adamlap@hotmail.com>
2026-08-22 00:19:13 +0200

    feat(cce): export continuous state field

--- nsa/runtime/__init__.py
+++ nsa/runtime/__init__.py
@@ -6,12 +6,15 @@ Trusted Cognitive Runtime for NSA Autonomous Execution.
 
 from .cce_adapter import SubstrateTransition, SubstrateTransitionConfig
 from .continuous_engine import CCEStatus, ContinuousCognitiveEngine
+from .continuous_state_field import ContinuousFieldStatus, ContinuousStateField
 from .engine import CognitiveRuntime, ExecutionContext
 from .typed_runtime import NSATypedRuntime, RuntimeGeneration
 
 __all__ = [
     "CCEStatus",
     "ContinuousCognitiveEngine",
+    "ContinuousFieldStatus",
+    "ContinuousStateField",
     "CognitiveRuntime",
     "ExecutionContext",
     "NSATypedRuntime",
