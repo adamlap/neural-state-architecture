80a409f7e769a3af63cf106f48538897c615bcfa
Adam Lapworth <adamlap@hotmail.com>
2026-09-16 01:43:13 +0200

    feat: export information seeking primitives

--- nsa/cognition/__init__.py
+++ nsa/cognition/__init__.py
@@ -1,20 +1,14 @@
 """Lightweight model-agnostic cognitive substrate primitives."""
-
 from .substrate import (
     CognitiveMetrics, CognitiveState, CognitiveSubstrate, CognitiveSwitches,
-    IntegrationGraph, Prediction, PredictionState, SelfModelState,
-    WorkspaceCandidate, WorkspaceState,
+    IntegrationGraph, Prediction, PredictionState, SelfModelState, WorkspaceCandidate, WorkspaceState,
 )
-from .interfaces import (
-    ActionCandidate, ActionSelector, BeliefUpdater, InformationGainModel,
-    PredictionError, Predictor,
-)
-from .deliberation import DeliberationDecision, UncertaintyDrivenDeliberator
+from .interfaces import ActionCandidate, ActionSelector, BeliefUpdater, InformationGainModel, PredictionError, Predictor
+from .deliberation import DeliberationDecision, InformationNeed, InformationSeekingPlanner, UncertaintyDrivenDeliberator
 
 __all__ = [
-    "CognitiveMetrics", "CognitiveState", "CognitiveSubstrate", "CognitiveSwitches",
-    "IntegrationGraph", "Prediction", "PredictionState", "SelfModelState",
-    "WorkspaceCandidate", "WorkspaceState", "ActionCandidate", "ActionSelector",
-    "BeliefUpdater", "InformationGainModel", "PredictionError", "Predictor",
-    "DeliberationDecision", "UncertaintyDrivenDeliberator",
+    "CognitiveMetrics", "CognitiveState", "CognitiveSubstrate", "CognitiveSwitches", "IntegrationGraph",
+    "Prediction", "PredictionState", "SelfModelState", "WorkspaceCandidate", "WorkspaceState",
+    "ActionCandidate", "ActionSelector", "BeliefUpdater", "InformationGainModel", "PredictionError", "Predictor",
+    "DeliberationDecision", "InformationNeed", "InformationSeekingPlanner", "UncertaintyDrivenDeliberator",
 ]
