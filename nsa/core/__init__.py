c53c0a704d65d1de4837a6c98c3f948feee85ff9
Adam Lapworth <adamlap@hotmail.com>
2026-09-16 01:45:13 +0200

    fix: preserve core authorization and safety exports

--- nsa/core/__init__.py
+++ nsa/core/__init__.py
@@ -1,23 +1,19 @@
 """Canonical NSA typed neural state core."""
-
 from nsa.core.heterogeneous_algebra import (
-    BooleanDomain, CapabilityDomain, ConstraintSetDomain, EnumDomain,
-    HeterogeneousState, NumericRangeDomain, ProbabilityInterval,
-    ProbabilityIntervalDomain, TemporalWindow, TemporalWindowDomain,
+    BooleanDomain, CapabilityDomain, ConstraintSetDomain, EnumDomain, HeterogeneousState,
+    NumericRangeDomain, ProbabilityInterval, ProbabilityIntervalDomain, TemporalWindow, TemporalWindowDomain,
 )
-from nsa.core.state import (
-    CanonicalState, GoalState, HardState, ProvenanceState, SemanticState,
-    SoftState, StateKind, StateTransition,
-)
+from nsa.core.state import CanonicalState, GoalState, HardState, ProvenanceState, SemanticState, SoftState, StateKind, StateTransition
 from nsa.core.transition import TransitionProposal, TransitionReceipt, TransitionValidator, state_digest
 from nsa.core.transition_cone import TransitionCone, TransitionDirection
+from nsa.core.capabilities import CapabilityAuthority, CapabilityToken, TrustThermodynamicsVector, TrustTier
+from nsa.core.safety_kernel import ImmutableSafetyKernel, InvariantResult, KernelEvaluationResult, KernelVerdict
 
 __all__ = [
-    "BooleanDomain", "CapabilityDomain", "ConstraintSetDomain", "EnumDomain",
-    "HeterogeneousState", "NumericRangeDomain", "ProbabilityInterval",
-    "ProbabilityIntervalDomain", "TemporalWindow", "TemporalWindowDomain",
-    "TransitionCone", "TransitionDirection", "CanonicalState", "GoalState",
-    "HardState", "ProvenanceState", "SemanticState", "SoftState", "StateKind",
-    "StateTransition", "TransitionProposal", "TransitionReceipt", "TransitionValidator",
-    "state_digest",
+    "BooleanDomain", "CapabilityDomain", "ConstraintSetDomain", "EnumDomain", "HeterogeneousState", "NumericRangeDomain",
+    "ProbabilityInterval", "ProbabilityIntervalDomain", "TemporalWindow", "TemporalWindowDomain", "TransitionCone",
+    "TransitionDirection", "CanonicalState", "GoalState", "HardState", "ProvenanceState", "SemanticState", "SoftState",
+    "StateKind", "StateTransition", "TransitionProposal", "TransitionReceipt", "TransitionValidator", "state_digest",
+    "CapabilityAuthority", "CapabilityToken", "TrustThermodynamicsVector", "TrustTier", "ImmutableSafetyKernel",
+    "InvariantResult", "KernelEvaluationResult", "KernelVerdict",
 ]
