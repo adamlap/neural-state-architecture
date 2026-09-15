9f62082c15bbf9601e7d53b8bb83787e1e8797ac
Adam Lapworth <adamlap@hotmail.com>
2026-09-16 01:44:26 +0200

    feat: export Omega and six-layer adapters

--- nsa/cce/__init__.py
+++ nsa/cce/__init__.py
@@ -8,11 +8,14 @@ from .canonical_runtime import CanonicalCCERuntime, TickInput
 from .loop import CognitiveCycle, CognitiveLoop, default_prediction_error
 from .persistence import TrajectoryJournal, record_to_dict
 from .safety_adapter import ImmutableKernelGate, SafetyDecision
+from .omega_adapter import CanonicalOmegaAdapter, OmegaDimensions
+from .substrate_adapter import SixLayerCanonicalAdapter, SubstrateProposal
 
 __all__ = [
     "CCEStatus", "ContinuousCognitiveEngine", "CognitiveEvent", "EventKind",
     "CheckpointEnvelope", "CognitiveInputEvent", "CognitiveInputQueue", "StateCheckpointStore",
     "CognitiveTrajectory", "TrajectoryRecord", "CognitiveTransaction", "CognitiveTransactionEngine",
-    "CanonicalCCERuntime", "TickInput", "CognitiveCycle", "CognitiveLoop",
-    "default_prediction_error", "TrajectoryJournal", "record_to_dict", "ImmutableKernelGate", "SafetyDecision",
+    "CanonicalCCERuntime", "TickInput", "CognitiveCycle", "CognitiveLoop", "default_prediction_error",
+    "TrajectoryJournal", "record_to_dict", "ImmutableKernelGate", "SafetyDecision",
+    "CanonicalOmegaAdapter", "OmegaDimensions", "SixLayerCanonicalAdapter", "SubstrateProposal",
 ]
