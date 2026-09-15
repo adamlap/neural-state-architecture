e43efff3c01ab73c450e6c39c8ffb3cd5989ee69
Adam Lapworth <adamlap@hotmail.com>
2026-09-16 01:43:08 +0200

    feat: export canonical cognitive loop and journal

--- nsa/cce/__init__.py
+++ nsa/cce/__init__.py
@@ -1,15 +1,18 @@
 """Continuous Cognitive Engine public package."""
-
 from .engine import CCEStatus, ContinuousCognitiveEngine
 from .events import CognitiveEvent, EventKind
 from .lifecycle import CheckpointEnvelope, CognitiveInputEvent, CognitiveInputQueue, StateCheckpointStore
 from .trajectory import CognitiveTrajectory, TrajectoryRecord
 from .transaction import CognitiveTransaction, CognitiveTransactionEngine
 from .canonical_runtime import CanonicalCCERuntime, TickInput
+from .loop import CognitiveCycle, CognitiveLoop, default_prediction_error
+from .persistence import TrajectoryJournal, record_to_dict
+from .safety_adapter import ImmutableKernelGate, SafetyDecision
 
 __all__ = [
     "CCEStatus", "ContinuousCognitiveEngine", "CognitiveEvent", "EventKind",
     "CheckpointEnvelope", "CognitiveInputEvent", "CognitiveInputQueue", "StateCheckpointStore",
     "CognitiveTrajectory", "TrajectoryRecord", "CognitiveTransaction", "CognitiveTransactionEngine",
-    "CanonicalCCERuntime", "TickInput",
+    "CanonicalCCERuntime", "TickInput", "CognitiveCycle", "CognitiveLoop",
+    "default_prediction_error", "TrajectoryJournal", "record_to_dict", "ImmutableKernelGate", "SafetyDecision",
 ]
