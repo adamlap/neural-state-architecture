d31550175bdd14ef401303eba9d1ca3971e348f7
Adam Lapworth <adamlap@hotmail.com>
2026-09-16 01:31:41 +0200

    feat: expose transactional CCE primitives

--- nsa/cce/__init__.py
+++ nsa/cce/__init__.py
@@ -6,13 +6,22 @@ submodules so ``import nsa`` remains lightweight.
 """
 
 from .engine import CCEStatus, ContinuousCognitiveEngine
+from .events import CognitiveEvent, EventKind
 from .lifecycle import CheckpointEnvelope, CognitiveInputEvent, CognitiveInputQueue, StateCheckpointStore
+from .trajectory import CognitiveTrajectory, TrajectoryRecord
+from .transaction import CognitiveTransaction, CognitiveTransactionEngine
 
 __all__ = [
     "CCEStatus",
     "ContinuousCognitiveEngine",
+    "CognitiveEvent",
+    "EventKind",
     "CheckpointEnvelope",
     "CognitiveInputEvent",
     "CognitiveInputQueue",
     "StateCheckpointStore",
+    "CognitiveTrajectory",
+    "TrajectoryRecord",
+    "CognitiveTransaction",
+    "CognitiveTransactionEngine",
 ]
