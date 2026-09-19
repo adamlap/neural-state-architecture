64a84305540b317063a865a07dbcfc4b24919f38
Adam Lapworth <adamlap@hotmail.com>
2026-09-20 00:38:40 +0200

    feat(residency): export execution instrumentation

--- nsa/residency/__init__.py
+++ nsa/residency/__init__.py
@@ -3,5 +3,5 @@ from nsa.residency.types import MemoryTier, ResidencyState, NeuralRegion, Reside
 from nsa.residency.policy import ResidencyPolicy, ResidencyDecision
 from nsa.residency.cache import ResidencyCache
 from nsa.residency.predictor import ResidencyPredictor, HeuristicResidencyPredictor
-from nsa.residency.manager import NeuralResidencyManager
-__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager"]
+from nsa.residency.manager import NeuralResidencyManager\nfrom nsa.residency.instrumentation import instrument_decoder_layers
+__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers"]
