d37eca7f13b553869c1dd972dc2b0cd66ae1a3a1
Adam Lapworth <adamlap@hotmail.com>
2026-09-20 00:43:37 +0200

    fix: repair residency exports and public lazy import

--- nsa/residency/__init__.py
+++ nsa/residency/__init__.py
@@ -3,7 +3,17 @@ from nsa.residency.types import MemoryTier, ResidencyState, NeuralRegion, Reside
 from nsa.residency.policy import ResidencyPolicy, ResidencyDecision
 from nsa.residency.cache import ResidencyCache
 from nsa.residency.predictor import ResidencyPredictor, HeuristicResidencyPredictor
-from nsa.residency.manager import NeuralResidencyManager\nfrom nsa.residency.instrumentation import instrument_decoder_layers\nfrom nsa.residency.controller import ActiveResidencyController, PrefetchTask
+from nsa.residency.manager import NeuralResidencyManager
+from nsa.residency.instrumentation import instrument_decoder_layers
+from nsa.residency.controller import ActiveResidencyController, PrefetchTask
 from nsa.residency.cognitive import cognitive_state_features
 from nsa.residency.learned import OnlineResidencyPredictor
-__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers","ActiveResidencyController","PrefetchTask","cognitive_state_features","OnlineResidencyPredictor"]
+from nsa.residency.trace import ResidencyTrace
+
+__all__ = [
+    "MemoryTier", "ResidencyState", "NeuralRegion", "ResidencyEvent", "ResidencySnapshot",
+    "ResidencyPolicy", "ResidencyDecision", "ResidencyCache", "ResidencyPredictor",
+    "HeuristicResidencyPredictor", "NeuralResidencyManager", "instrument_decoder_layers",
+    "ActiveResidencyController", "PrefetchTask", "cognitive_state_features",
+    "OnlineResidencyPredictor", "ResidencyTrace",
+]
