b207ab07bd79b4dd0c0f0b86fedb40ca5bcb1789
Adam Lapworth <adamlap@hotmail.com>
2026-09-20 00:41:01 +0200

    feat(residency): export active controller

--- nsa/residency/__init__.py
+++ nsa/residency/__init__.py
@@ -3,5 +3,5 @@ from nsa.residency.types import MemoryTier, ResidencyState, NeuralRegion, Reside
 from nsa.residency.policy import ResidencyPolicy, ResidencyDecision
 from nsa.residency.cache import ResidencyCache
 from nsa.residency.predictor import ResidencyPredictor, HeuristicResidencyPredictor
-from nsa.residency.manager import NeuralResidencyManager\nfrom nsa.residency.instrumentation import instrument_decoder_layers
-__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers"]
+from nsa.residency.manager import NeuralResidencyManager\nfrom nsa.residency.instrumentation import instrument_decoder_layers\nfrom nsa.residency.controller import ActiveResidencyController, PrefetchTask
+__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers","ActiveResidencyController","PrefetchTask"]
