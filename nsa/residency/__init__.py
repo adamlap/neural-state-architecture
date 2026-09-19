c8bba4fbe97e92e35c66f9ea290b07594a04e47d
Adam Lapworth <adamlap@hotmail.com>
2026-09-20 00:41:23 +0200

    feat(residency): export learned predictor

--- nsa/residency/__init__.py
+++ nsa/residency/__init__.py
@@ -5,4 +5,5 @@ from nsa.residency.cache import ResidencyCache
 from nsa.residency.predictor import ResidencyPredictor, HeuristicResidencyPredictor
 from nsa.residency.manager import NeuralResidencyManager\nfrom nsa.residency.instrumentation import instrument_decoder_layers\nfrom nsa.residency.controller import ActiveResidencyController, PrefetchTask
 from nsa.residency.cognitive import cognitive_state_features
-__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers","ActiveResidencyController","PrefetchTask","cognitive_state_features"]
+from nsa.residency.learned import OnlineResidencyPredictor
+__all__ = ["MemoryTier","ResidencyState","NeuralRegion","ResidencyEvent","ResidencySnapshot","ResidencyPolicy","ResidencyDecision","ResidencyCache","ResidencyPredictor","HeuristicResidencyPredictor","NeuralResidencyManager","instrument_decoder_layers","ActiveResidencyController","PrefetchTask","cognitive_state_features","OnlineResidencyPredictor"]
